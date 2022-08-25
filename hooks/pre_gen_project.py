7e3710a46762fcd94e788d284ab3029f7405f66f
Harshad Hegde <hegdehb@gmail.com>
2022-08-25 13:11:06 -0500

    uncommented sys.exit() code

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,7 +1,7 @@
 """Code to run before generating the project."""
 
 import re
-# import sys
+import sys
 
 MODULE_REGEX = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
 
@@ -12,12 +12,12 @@ if not MODULE_REGEX.match(project_name):
         invalid_char = "-"
     elif " " in project_name:
         invalid_char = " "
-    # else:
-    #     print(
-    #         f'ERROR: {project_name} is not a valid Python module name. Try again with a valid project name.'
-    #     )
-        # # Exit to cancel project
-        # sys.exit(1)
+    else:
+        print(
+            f'ERROR: {project_name} is not a valid Python module name. Try again with a valid project name.'
+        )
+        # Exit to cancel project
+        sys.exit(1)
     print(
         f'WARNING: {project_name} is not a valid Python module name. Using "_" instead of {invalid_char} for module name.'
     )
