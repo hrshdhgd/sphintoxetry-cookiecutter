937da40ff79e580c0cfdc4a34a2c01e0f8e8a4c7
Harshad Hegde <hegdehb@gmail.com>
2022-08-25 11:39:25 -0500

    remove boilerplate post_gen code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,16 +1,1 @@
 """Code to run after generating the project."""
-# import os
-
-# REMOVE_PATHS = [
-#     '{% if cookiecutter.packaging != "pip" %} requirements.txt {% endif %}',
-#     '{% if cookiecutter.packaging != "poetry" %} poetry.lock {% endif %}',
-# ]
-
-
-# for path in REMOVE_PATHS:
-#     path = path.strip()
-#     if path and os.path.exists(path):
-#         if os.path.isdir(path):
-#             os.rmdir(path)
-#         else:
-#             os.unlink(path)
