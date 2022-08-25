b4434d3267be13c6b172addb32a537be9c5006df
Harshad Hegde <hegdehb@gmail.com>
2022-08-25 11:37:22 -0500

    commented boilerplpate code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,16 +1,16 @@
 """Code to run after generating the project."""
-import os
+# import os
 
-REMOVE_PATHS = [
-    '{% if cookiecutter.packaging != "pip" %} requirements.txt {% endif %}',
-    '{% if cookiecutter.packaging != "poetry" %} poetry.lock {% endif %}',
-]
+# REMOVE_PATHS = [
+#     '{% if cookiecutter.packaging != "pip" %} requirements.txt {% endif %}',
+#     '{% if cookiecutter.packaging != "poetry" %} poetry.lock {% endif %}',
+# ]
 
 
-for path in REMOVE_PATHS:
-    path = path.strip()
-    if path and os.path.exists(path):
-        if os.path.isdir(path):
-            os.rmdir(path)
-        else:
-            os.unlink(path)
+# for path in REMOVE_PATHS:
+#     path = path.strip()
+#     if path and os.path.exists(path):
+#         if os.path.isdir(path):
+#             os.rmdir(path)
+#         else:
+#             os.unlink(path)
