ba2a340a67f848a4c9cff1f723fed3fdcef1493b
Harshad Hegde <hegdehb@gmail.com>
2022-08-25 12:01:52 -0500

    test

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -8,9 +8,10 @@ MODULE_REGEX = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
 project_name = '{{ cookiecutter.project_name}}'
 
 if not MODULE_REGEX.match(project_name):
-    for match in re.split(r"'.*?'", project_name):
-        print(
-            f'WARNING: {project_name} is not a valid Python module name. Using "_" instead of {match} for module name.'
-        )
+    for match in re.split(r"'?!'^[_a-zA-Z][_a-zA-Z0-9]+$", project_name):
+        # print(
+        #     f'WARNING: {project_name} is not a valid Python module name. Using "_" instead of {match} for module name.'
+        # )
+        print(match)
 
     
